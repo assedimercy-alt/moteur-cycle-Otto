2f2691480b64b6d249f17a9bbc4a4446b35ed2d0
assedimercy-alt <assedimercy@gmail.com>
2026-02-08 02:47:53 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import numpy as np
-import plotly.graph_objects as go
+import plotly.graph_objects as go 
 
 # --- CONFIGURATION ---
 st.set_page_config(page_title="Simulateur EPL", layout="wide")
